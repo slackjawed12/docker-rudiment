1218e6812523c228dd8620691b1ff8cee8df36d1
narustudio <slackjawed12@gmail.com>
2024-01-24 22:12:09 +0900

    chapter4 - node.js 소스코드

---
+++ ch04/access-log/src/config/logConfig.js
@@ -0,0 +1,11 @@
+const { format, transports } = require("winston");
+var logConfig = (module.exports = {});
+
+logConfig.options = {
+  format: format.combine(format.splat(), format.simple()),
+  transports: [
+    new transports.Console({
+      level: "info",
+    }),
+  ],
+};
